3c5f3a36a8df98719571f8906e725719cb810001
방태연 <imbhang3@naver.com>
2024-03-15 13:32:01 +0900

    ignore추가

--- assets/js/key.js
+++
@@ -1,3 +0,0 @@
-const serviceKey =
-  "v5mkpfYxfADBELd%2FyPcHkJYZvvF6Mrl%2B7qKsSErjG47yff0oH08Ane6KxY%2BD8wmYdW%2FyZKhy9fC2flpHwzQySg%3D%3D";
-// const kakaoKey = "----- 발급받은 KAKAO API KEY를 입력하세요 -----";
